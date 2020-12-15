b0333f5e160bf6d9d7000d137a0d00f204513dea
solbencas <cecibenitezca@gmail.com>
2020-12-15 16:26:35 -0300

    Add test-driven-development-branch

---
+++ src/assets/tests/index.test.js
@@ -0,0 +1,5 @@
+import * as func from '../index.js';
+
+test('testing function sum', () => {
+  expect(func.sum(1, 2)).toBe(3);
+})
--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import './assets/styles.css';
 import { render } from './modules/render_save';
 
 render();
+
+export const sum = (a, b) => {
+  return a + b;
+}
+
+
+
